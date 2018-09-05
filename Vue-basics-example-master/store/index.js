da414ebc87406dda205714dc6e569973d678edb6
ncy1224 <985767498@qq.com>
2018-09-05 15:47:54 +0800

    version1.0

---
+++ Vue-basics-example-master/store/index.js
@@ -0,0 +1,46 @@
+
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+ // 创建一个store仓库
+ const store  = new Vuex.Store({
+    //  声明 共享状态值
+    state:{
+        count:0
+    },
+
+    // 定义获取 state，getters 对象
+    getters: {
+        getCount: state => {
+            return state.count
+        }
+    },
+
+    // 定义唯一修改 state 的途径
+    mutations: {
+        add (state) {
+            state.count++
+        },
+        jian (state) {
+            state.count--
+        },
+        sum(state){
+        	state.count=state.count*2
+        }
+    },
+
+    // actions 异步 修改 state , 其实是提交一个 mutations 
+    actions: {
+        addAction (context) {
+            context.commit('add')
+        },
+        jianAction (context) {
+            context.commit('jian')
+        }
+    }
+    
+ })
+
+ export default store;
